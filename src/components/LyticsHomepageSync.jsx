d7a01756a5781d7a49ef4c0aae765793e0d2f5b5
Iain Murphy <murphyslawlytics@icloud.com>
2026-05-08 17:33:32 +0100

    simple debug for homepagesync output to troubleshoot

--- src/components/LyticsHomepageSync.jsx
+++ src/components/LyticsHomepageSync.jsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
 import { useTopUnpurchasedProduct } from "@/context/lyticsTracking";
 
 export default function LyticsHomepageSync() {
+  useEffect(() => {
+    console.log("LyticsHomepageSync mounted"); // ← add this
+  }, []);
+  
   useTopUnpurchasedProduct();
   return null;
 }
